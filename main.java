2efb38b95b88f674b02f935625b52250dcb3a1c8
gherman7755 <58359494+gherman7755@users.noreply.github.com>
2021-02-27 13:47:49 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,34 +3,25 @@ package com.company;
 import java.util.Arrays;
 import java.lang.*;
 
-
 public class Main {
 
-    public static void main(String[] args) {
-        long start = System.nanoTime();
-        boolean[] a = algo1(1000000);
-        long end = System.nanoTime();
-        System.out.println("Time elapsed: " + (end - start) / 1000000 + " ms.");
+    public static void main(String[] args){
+        // n - range from 0 to n
+        // multiple - for increasing range
+        // repeats - how much time do you want n to increase
+        // algo - number of algorithm from 1 - 5.
+        elapsed(10, 10, 5, 3);
+        // if you want check if algorithms work
+        // just uncomment next code and change algo1 to which you want
 
-        start = System.nanoTime();
-        boolean[] b = algo2(1000000);
-        end = System.nanoTime();
-        System.out.println("Time elapsed: " + (end - start) / 1000000 + " ms.");
-
-        start = System.nanoTime();
-        boolean[] c = algo3(1000000);
-        end = System.nanoTime();
-        System.out.println("Time elapsed: " + (end - start) / 1000000 + " ms.");
-
-        start = System.nanoTime();
-        boolean[] d = algo4(1000000);
-        end = System.nanoTime();
-        System.out.println("Time elapsed: " + (end - start) / 1000000 + " ms.");
-
-        start = System.nanoTime();
-        boolean[] e = algo5(1000000);
-        end = System.nanoTime();
-        System.out.println("Time elapsed: " + (end - start) / 1000000 + " ms.");
+        /*
+        boolean[] array = algo1(100);
+        for(int i = 0; i < 100; i++){
+            if(array[i]){
+                System.out.print(i + " ");
+            }
+        }
+        */
     }
 
     public static boolean[] algo1(int n){
@@ -92,7 +83,7 @@ public class Main {
             if(c[i]){
                 j = i + 1;
                 while(j <= n){
-                    if(j % i == 0){
+                    if(j % i == 0){ // there was a mistake in word file - "if(i % j)..."
                         c[j] = false;
                     }
                     j++;
@@ -114,9 +105,9 @@ public class Main {
         int j;
 
         while(i <= n){
-            j = 2;
+            j = 2; // here also was mistake - "j = 1"
             while(j < i){
-                if(i % j == 0){
+                if(i % j == 0){ // and here - "if(j % i == 0)"
                     c[i] = false;
                 }
                 j++;
@@ -139,7 +130,7 @@ public class Main {
         while(i <= n){
             j = 2;
             while(j <= Math.sqrt(i)){
-                if(i % j == 0){
+                if(i % j == 0){ // and here was the same mistake
                     c[i] = false;
                 }
                 j++;
@@ -147,5 +138,75 @@ public class Main {
             i++;
         }
         return c;
+    }
+
+    public static long elapsed(int n, int multiple, int repeats, int algo){
+        switch (algo){
+            case 1:
+                for(int i = 0; i < repeats; i++){
+                    long start = System.nanoTime();
+                    algo1(n);
+                    long end = System.nanoTime();
+                    float result = (end - start) / 1000000.0f;
+                    System.out.println("Time elapsed: " + result + " ms.");
+
+                    n *= multiple;
+                }
+                break;
+
+            case 2:
+                for(int i = 0; i < repeats; i++){
+                    long start = System.nanoTime();
+                    algo2(n);
+                    long end = System.nanoTime();
+                    float result = (end - start) / 1000000.0f;
+                    System.out.println("Time elapsed: " + result + " ms.");
+
+                    n *= multiple;
+                }
+                break;
+
+            case 3:
+                for(int i = 0; i < repeats; i++){
+                    long start = System.nanoTime();
+                    algo3(n);
+                    long end = System.nanoTime();
+                    float result = (end - start) / 1000000.0f;
+                    System.out.println("Time elapsed: " + result + " ms.");
+
+                    n *= multiple;
+                }
+                break;
+
+            case 4:
+                for(int i = 0; i < repeats; i++){
+                    long start = System.nanoTime();
+                    algo4(n);
+                    long end = System.nanoTime();
+                    float result = (end - start) / 1000000.0f;
+                    System.out.println("Time elapsed: " + result + " ms.");
+
+                    n *= multiple;
+                }
+                break;
+
+            case 5:
+                for(int i = 0; i < repeats; i++){
+                long start = System.nanoTime();
+                algo5(n);
+                long end = System.nanoTime();
+                float result = (end - start) / 1000000.0f;
+                System.out.println("Time elapsed: " + result + " ms.");
+
+                n *= multiple;
+            }
+                break;
+
+            default:
+                System.out.println("No, no, no! Don't try to break my code!!!");
+                break;
+        }
+
+        return 0;
     }
 }
